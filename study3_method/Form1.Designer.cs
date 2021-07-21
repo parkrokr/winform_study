a5abfd4e038b9260307eb22714109bb2d4ba4538
parkrokr <parkrokr@gmail.com>
2021-07-21 17:35:30 +0900

    study code

--- study3_method/Form1.Designer.cs
+++ study3_method/Form1.Designer.cs
@@ -29,13 +29,110 @@ namespace study3_method
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.tboxNum1 = new System.Windows.Forms.TextBox();
+            this.tboxNum2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(37, 32);
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(361, 27);
+            this.tboxResult.TabIndex = 0;
+            this.tboxResult.Text = "0";
+            // 
+            // tboxNum1
+            // 
+            this.tboxNum1.Location = new System.Drawing.Point(37, 80);
+            this.tboxNum1.Name = "tboxNum1";
+            this.tboxNum1.Size = new System.Drawing.Size(169, 27);
+            this.tboxNum1.TabIndex = 1;
+            this.tboxNum1.Text = "0";
+            // 
+            // tboxNum2
+            // 
+            this.tboxNum2.Location = new System.Drawing.Point(231, 80);
+            this.tboxNum2.Name = "tboxNum2";
+            this.tboxNum2.Size = new System.Drawing.Size(167, 27);
+            this.tboxNum2.TabIndex = 2;
+            this.tboxNum2.Text = "0";
+            this.tboxNum2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(37, 140);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(76, 38);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(130, 140);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(76, 38);
+            this.btnMinus.TabIndex = 4;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(231, 140);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(76, 38);
+            this.btnMulti.TabIndex = 5;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(322, 140);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(76, 38);
+            this.btnDivision.TabIndex = 6;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(436, 235);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.tboxNum2);
+            this.Controls.Add(this.tboxNum1);
+            this.Controls.Add(this.tboxResult);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.TextBox tboxNum1;
+        private System.Windows.Forms.TextBox tboxNum2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDivision;
     }
 }
 
--- study3_method/Form1.cs
+++ study3_method/Form1.cs
@@ -17,5 +17,60 @@ namespace study3_method
             InitializeComponent();
         }
 
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                   
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tboxNum1.Text);
+            int iNumB = int.Parse(tboxNum2.Text);
+
+            int iResult = iNumA + iNumB;
+
+            tboxResult.Text = iResult.ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tboxNum1.Text);
+            int iNumB = int.Parse(tboxNum2.Text);
+
+            int iResult = iNumA - iNumB;
+
+            tboxResult.Text = iResult.ToString();
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tboxNum1.Text);
+            int iNumB = int.Parse(tboxNum2.Text);
+
+            int iResult = iNumA * iNumB;
+
+            tboxResult.Text = iResult.ToString();
+        }
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tboxNum1.Text);
+            int iNumB = int.Parse(tboxNum2.Text);
+
+            int iResult = iNumA / iNumB;
+
+            tboxResult.Text = iResult.ToString();
+        }
+
+        private int ex(int iA, int iB)
+        {
+            return 0;
+        }
     }
 }
